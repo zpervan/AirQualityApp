57d4a577d022936dd5b1558f7a54e426f136bc99
Zvonimir Pervan <z.pervan65@gmail.com>
2020-04-04 22:48:49 +0200

    setup: Adding initial files
    Adding basic project files to run a simple "Hello, World!" application.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Udacity capstone project, 2020
+// Project: Air Quality in Croatia
+// Student: Zvonimir Pervan
+
+#include <iostream>
+
+int main() {
+  std::cout << "This is my first step..." << std::endl;
+  return 0;
+}
