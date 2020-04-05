b39faf2b37cee91f3b20535a29ad29a81ebda983
Zvonimir Pervan <z.pervan65@gmail.com>
2020-04-05 10:58:24 +0200

    feature: Add GoogleTest and skeleton test for DataScraper functionality

---
+++ Utility/DataScraper/Test/data_scraper_test.cpp
@@ -0,0 +1,17 @@
+// Udacity capstone project, 2020
+// Air Quality in Croatia
+// Student: Zvonimir Pervan
+
+#include "../data_scraper.h"
+
+#include <gtest/gtest.h>
+
+TEST(DataScraperTest, Run)
+{
+  EXPECT_TRUE(false);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
