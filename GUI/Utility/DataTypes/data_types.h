ac57f9f8ff86c3705dd050e6270d82993bd18b8c
Zvonimir Pervan <z.pervan65@gmail.com>
2020-04-15 22:26:24 +0200

    Extend DataTypes with Pollutant enum class

--- GUI/Utility/DataTypes/data_types.h
+++ GUI/Utility/DataTypes/data_types.h
@@ -46,5 +46,10 @@ private:
   UrlComponents() = default;
 };
 
-enum class Pollutants { CARBON_MONOXIDE = 3, OZONE = 32, BENZEN = 31 };
+enum class Pollutant {
+  UNKNOWN = 0,
+  CARBON_MONOXIDE = 3,
+  OZONE = 32,
+  BENZEN = 31
+};
 #endif // AIRQUALITYAPP_DATA_TYPES_H
